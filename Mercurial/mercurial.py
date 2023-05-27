2c86251bd0a180b05783a50b7d993dff77b74812
Ranveer Chaudhary <64607338+ranveerchaudhary@users.noreply.github.com>
2023-05-27 17:22:11 +0530

    Add files via upload

--- Mercurial/mercurial.py
+++ Mercurial/mercurial.py
@@ -5,20 +5,30 @@ import pandas as pd
 import numpy as np
 import warnings
 import random as rd
+from sklearn.cluster import KMeans
 
 warnings.filterwarnings("ignore")
 
 df1 = pd.read_csv("dataset.csv")
 
+kmeansplus = KMeans(n_clusters=4, init='k-means++', random_state=42)
+data = df1.select_dtypes(include='number')
+kmeansplus.fit(data)
+data = df1.select_dtypes(include='number')
+kmeansplus.fit(data)
+
 usedtracks = []
 
 def get_track():
+    #track_no = rd.choice(listr)
     track_no = rd.randint(0, len(df1.index))
     
     while (track_no in usedtracks):
         track_no = rd.randint(0, len(df1.index))
+        #track_no = rd.choice(listr)
     
     usedtracks.append(track_no)
+    #track_no = df1[df1['id'] == track_no].index[0]
     track = df1.iloc[track_no]
     track_name = track["name"]
     artist_name = track["artists"].strip('][').replace("\'", "").split(",")
@@ -28,25 +38,24 @@ def get_track():
 
 
 
-def recommend(songs_list):
+def recommend(songs_list, amount=5):
+    global data
     recommendations = []
+
     for song_name in songs_list:
-        distance = []
-        song = df1[(df1.name.str.lower() == song_name.lower())].head(1).values[0]
-        rec = df1[df1.name.str.lower() != song_name.lower()]
-        for songs in rec.values:
-            d = 0
-            for col in np.arange(len(rec.columns)):
-                if not col in [0, 1, 5, 6, 7]:
-                    d = d + np.absolute(float(song[col]) - float(songs[col]))
-            distance.append(d)
-        rec['distance'] = distance
-        rec = rec.sort_values('distance').head(2)
-        columns = ['artists','name']
-        recommendations.append(rec[columns])
+        song = df1[(df1.name.str.lower() == song_name.lower())].head(1).iloc[0]
+        song_features = song[data.columns]
+        cluster_label = kmeansplus.predict([song_features])[0]
+
+        cluster_indices = np.where(kmeansplus.labels_ == cluster_label)[0]
+        cluster_songs = df1.iloc[cluster_indices]
+        cluster_songs = cluster_songs[cluster_songs.name.str.lower() != song_name.lower()]
+
+        recommendations.append(cluster_songs) 
+
     recommendations = pd.concat(recommendations, ignore_index=True)
     reco = list(zip(recommendations["artists"].tail(), recommendations["name"].tail()))
-    
+
     artist = []
     music = []
     data = {}
@@ -55,8 +64,8 @@ def recommend(songs_list):
         music.append(reco[z][1])
     for q in range(len(artist)):
         artist[q] = artist[q].strip('][').replace("\'", "").split(",")
-    
+
     for k in range(len(artist)):
         data[music[k]] = artist[k]
-    
+
     return data
